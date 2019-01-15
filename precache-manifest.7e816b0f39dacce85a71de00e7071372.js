0d10686908e7ea9f74f7f604502bac0c5c5af16d
daveywavey16 <daveywavey16@msn.com>
2019-01-15 14:39:30 -0700

    Updates

---
+++ precache-manifest.7e816b0f39dacce85a71de00e7071372.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51a8c4cf43748326fadd",
+    "url": "/weezer-react-game/static/js/runtime~main.51a8c4cf.js"
+  },
+  {
+    "revision": "84d0ed7a8da3e0de2c01",
+    "url": "/weezer-react-game/static/js/main.84d0ed7a.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/weezer-react-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "84d0ed7a8da3e0de2c01",
+    "url": "/weezer-react-game/static/css/main.9f2df453.chunk.css"
+  },
+  {
+    "revision": "4533dcf523b5e955314fe0b224ca3fd4",
+    "url": "/weezer-react-game/index.html"
+  }
+];
